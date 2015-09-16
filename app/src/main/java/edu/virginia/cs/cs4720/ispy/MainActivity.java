933ced349b7b7fb0de18af43691ebc57a640962b
jtr4fh <jtr4fh@virginia.edu>
2015-09-15 21:12:36 -0400

    broken app

--- app/src/main/java/edu/virginia/cs/cs4720/ispy/MainActivity.java
+++ app/src/main/java/edu/virginia/cs/cs4720/ispy/MainActivity.java
@@ -1,16 +1,105 @@
 package edu.virginia.cs.cs4720.ispy;
 
-import android.support.v7.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1, b2;
+    Button camButt;
+    Button galButt;
+    TextView tv;
+    EditText et;
+    int TAKE_PIC = 1;
+    Uri imageUri;
+    ImageView ivImage;
+
+    List<String> myList;
+    String mCurrentPhotoPath;
+
+
+    GPS gps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivImage = (ImageView)findViewById(R.id.ivImage);
+        b1 = (Button) findViewById(R.id.button1);
+
+
+        b1.setOnClickListener( new View.OnClickListener() {
+            public void onClick(View view) {
+                et = (EditText)findViewById(R.id.edit);
+                tv = (TextView)findViewById(R.id.textView2);
+                tv.setText("Welcome "+et.getText().toString()+"!");
+            }
+        });
+
+        b2 = (Button) findViewById(R.id.button);
+
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                gps = new GPS(MainActivity.this);
+
+                if (gps.canGetLocation()) {
+
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+
+                    Toast.makeText(getApplicationContext(), "your location is: \nLat" + latitude + "\nLon" + longitude, Toast.LENGTH_LONG).show();
+                } else {
+                    gps.showSettingsAlert();
+                }
+            }
+        });
+
+        camButt = (Button)findViewById(R.id.myButt);
+
+        camButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(MainActivity.this, CameraPhotoCapture.class);
+                startActivity(i);
+            }
+        });
+
+        galButt = (Button)findViewById(R.id.galButt);
+        galButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(MainActivity.this, AndroidCustomGallery.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
